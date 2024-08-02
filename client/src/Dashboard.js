c8e985644f43ae20832460f2c9e3fdf65be18512
Am10aN16 <2020071013@mmmut.ac.in>
2024-08-03 01:34:00 +0530

    code changes

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
 
     useEffect(() => {
         // Create WebSocket connection
-        const socket = new WebSocket('ws://loxone-light.onrender.com/');
+        const socket = new WebSocket('https://loxone-light.onrender.com/');
         setWs(socket);
 
         // Connection opened
